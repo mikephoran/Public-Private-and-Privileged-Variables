37f8ab200935ecb21d33ddd211979a8921d65e6e
Mike Horan <mikephoran@gmail.com>
2014-04-29 13:02:55 -0700

    Update private members test.js

--- private members test.js
+++ private members test.js
@@ -1,7 +1,7 @@
 function Cat (c) {
 	this.color = c;
 	var count = 0;
-	var that = this;  //Can't think of when I'd use this, but Doug Crockford says this is a workaround for an error in the ECMAScript Language Specificationw hich causes this to be set incorrectly for inner functions
+	var that = this;  //Can't think of when I'd use this, but Doug Crockford says this is a workaround for an error in the ECMAScript Language Specification which causes this to be set incorrectly for inner functions
 	function secret() {
 		if (count < 4) {
 			console.log(count);
@@ -18,5 +18,8 @@ var Garf = new Cat("orange");
 
 Garf.priv();
 
+//Couldn't figure out why count wasn't going up, until I thought about what cntrl-b was actually doing.  Then came up with this elegant solution:
+
+Garf.priv();Garf.priv();Garf.priv();Garf.priv();Garf.priv();Garf.priv();Garf.priv();
 
 
